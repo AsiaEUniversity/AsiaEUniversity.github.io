cf5a241b33137facef634b2632303e8a2f693b39
AsiaEUniversity <99003287+AsiaEUniversity@users.noreply.github.com>
2022-02-04 14:10:33 +0800

    Add files via upload

--- admin/enroll-history.php
+++ admin/enroll-history.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
     {   
 header('location:index.php');
 }
@@ -27,7 +28,7 @@ else{
 <body>
 <?php include('includes/header.php');?>
     <!-- LOGO HEADER END-->
-<?php if($_SESSION['alogin']!="")
+<?php if($_SESSION['login']!="")
 {
  include('includes/menubar.php');
 }
@@ -53,21 +54,19 @@ else{
                             <div class="table-responsive table-bordered">
                                 <table class="table">
                                     <thead>
-                                        <tr>
+                                       
                                             <th>#</th>
-                                                 <th>Student Name </th>
-                                                    <th>Student Reg no </th>
-                                            <th>Course Name </th>
-                                            <th>Department </th>
-                                            
+                                            <th>Course</th>
+                                            <th>Session </th>
+                                            <th> Department</th>
                                                 <th>Semester</th>
                                              <th>Enrollment Date</th>
                                              <th>Action</th>
-                                        </tr>
+                                        
                                     </thead>
                                     <tbody>
 <?php
-$sql=mysqli_query($con,"select courseenrolls.course as cid, course.courseName as courname,session.session as session,department.department as dept,courseenrolls.enrollDate as edate ,semester.semester as sem,students.studentName as sname,students.StudentRegno as sregno from courseenrolls join course on course.id=courseenrolls.course join session on session.id=courseenrolls.session join department on department.id=courseenrolls.department   join semester on semester.id=courseenrolls.semester join students on students.StudentRegno=courseenrolls.studentRegno ");
+$sql=mysqli_query($con,"select courseenrolls.course as cid, course.courseName as courname,session.session as session,department.department as dept,courseenrolls.enrollDate as edate ,semester.semester as sem from courseenrolls join course on course.id=courseenrolls.course join session on session.id=courseenrolls.session join department on department.id=courseenrolls.department join level on level.id=courseenrolls.level  join semester on semester.id=courseenrolls.semester  where courseenrolls.studentRegno='".$_SESSION['login']."'");
 $cnt=1;
 while($row=mysqli_fetch_array($sql))
 {
@@ -76,11 +75,9 @@ while($row=mysqli_fetch_array($sql))
 
                                         <tr>
                                             <td><?php echo $cnt;?></td>
-                                              <td><?php echo htmlentities($row['sname']);?></td>
-                                            <td><?php echo htmlentities($row['sregno']);?></td>
                                             <td><?php echo htmlentities($row['courname']);?></td>
+                                            <td><?php echo htmlentities($row['session']);?></td>
                                             <td><?php echo htmlentities($row['dept']);?></td>
-                                          
                                             <td><?php echo htmlentities($row['sem']);?></td>
                                              <td><?php echo htmlentities($row['edate']);?></td>
                                             <td>
